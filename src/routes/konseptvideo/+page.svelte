9fc650f40bcd95500115c34ef8df811dc46d7a97
sveste123 <sverrestenbaek@icloud.com>
2024-12-19 14:42:05 +0100

    Testing pull-request

--- src/routes/konseptvideo/+page.svelte
+++ src/routes/konseptvideo/+page.svelte
@@ -1,5 +1,5 @@
 <div class="video-container">
-    <iframe src="https://www.youtube.com/embed/KV9tyDtX304" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>Konseptvideo</iframe>
+    <iframe title="Konseptvideo" src="https://www.youtube.com/embed/KV9tyDtX304" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>Konseptvideo</iframe>
 </div>
 <style>
     /* Konseptvideo */
