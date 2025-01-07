7c056bc906adad77d7c00fffe07b13726a0a526c
sveste123 <sverrestenbaek@icloud.com>
2025-01-07 15:40:40 +0100

    Januar - Ukedag

--- src/routes/forestillinger/+page.svelte
+++ src/routes/forestillinger/+page.svelte
@@ -3,12 +3,12 @@
     <p><strong>Premiere:</strong> 3. Januar</p>
     <p><strong>Forestillingene spilles i Aulaen på Lillestrøm VGS disse datoene:</strong></p>
     <ul>
-      <li>03. Januar (kl. 19:00)</li>
-      <li>04. Januar (kl. 14:00 og kl. 18:00)</li>
-      <li>05. Januar (kl. 14:00 og kl. 18:00)</li>
-      <li>07. Januar (kl. 18:00)</li>
-      <li>08. Januar (kl. 18:00)</li>
-      <li>10. Januar (kl. 18:00)</li>
-      <li>11. Januar (kl. 14:00 og kl. 18:00)</li>
+      <li>Fredag 3. (kl. 19:00)</li>
+      <li>Lørdag 4. (kl. 14:00 og kl. 18:00)</li>
+      <li>Søndag 5. (kl. 14:00 og kl. 18:00)</li>
+      <li>Tirsdag 7. (kl. 18:00)</li>
+      <li>Onsdag 8. (kl. 18:00)</li>
+      <li>Fredag 10. (kl. 18:00)</li>
+      <li>Lørdag 11. (kl. 14:00 og kl. 18:00)</li>
     </ul>
   </div>
